d458c8977d4d3ae94c4f808a7e196e176e72cd6e
Ariel Borochov <borochov@student.american.edu>
2013-05-29 21:21:24 -0400

    Initial Commit

---
+++ Logickaker/AB1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AB1AppDelegate.h
+//  Logickaker
+//
+//  Created by Michael Black on 5/29/13.
+//  Copyright (c) 2013 American University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AB1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Logickaker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Logickaker
+//
+//  Created by Michael Black on 5/29/13.
+//  Copyright (c) 2013 American University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AB1AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AB1AppDelegate class]));
+    }
+}
