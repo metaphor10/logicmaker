46ea31971622d55ae80e40f1e81225492cf7860f
Ariel Borochov <borochov@student.american.edu>
2013-06-11 00:05:24 -0400

    Half way done got the scrollview and the draw view to communicate there is SQLlite structure to save the circuit components

--- AB1ComponentCircuit.h
+++ AB1ComponentCircuit.h
@@ -2,26 +2,29 @@
 //  AB1ComponentCircuit.h
 //  Logickaker
 //
-//  Created by Ariel Borochov on 5/31/13.
+//  Created by Michael Black on 6/7/13.
 //  Copyright (c) 2013 American University. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+@class AB1ComponentType;
 
 @interface AB1ComponentCircuit : NSManagedObject
 
+@property (nonatomic) int64_t bits;
+@property (nonatomic, retain) NSString * description1;
+@property (nonatomic) BOOL highlighted;
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic) double orderingvalue;
+@property (nonatomic) BOOL selected;
+@property (nonatomic, retain) NSString * type;
+@property (nonatomic) int64_t value;
 @property (nonatomic) int16_t xcoor;
 @property (nonatomic) int16_t ycoor;
-@property (nonatomic, retain) NSString * name;
-@property (nonatomic, retain) NSString * description1;
-@property (nonatomic) BOOL selected;
-@property (nonatomic) BOOL highlighted;
-@property (nonatomic, retain) NSString * type;
-@property (nonatomic) int64_t bits;
-@property (nonatomic) int64_t value;
-@property (nonatomic) double orderingvalue;
+@property (nonatomic) int32_t xcoor2;
+@property (nonatomic) int32_t ycoor2;
 @property (nonatomic, retain) NSManagedObject *assetType;
 
 @end
--- AB1ComponentCircuit.m
+++ AB1ComponentCircuit.m
@@ -2,25 +2,28 @@
 //  AB1ComponentCircuit.m
 //  Logickaker
 //
-//  Created by Ariel Borochov on 5/31/13.
+//  Created by Michael Black on 6/7/13.
 //  Copyright (c) 2013 American University. All rights reserved.
 //
 
 #import "AB1ComponentCircuit.h"
 
 
+
 @implementation AB1ComponentCircuit
 
+@dynamic bits;
+@dynamic description1;
+@dynamic highlighted;
+@dynamic name;
+@dynamic orderingvalue;
+@dynamic selected;
+@dynamic type;
+@dynamic value;
 @dynamic xcoor;
 @dynamic ycoor;
-@dynamic name;
-@dynamic description1;
-@dynamic selected;
-@dynamic highlighted;
-@dynamic type;
-@dynamic bits;
-@dynamic value;
-@dynamic orderingvalue;
+@dynamic xcoor2;
+@dynamic ycoor2;
 @dynamic assetType;
 
 @end
